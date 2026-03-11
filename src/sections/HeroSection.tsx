e195b6e81bfd1e3c61cd28e2b68e10217feb5371
salmeenbm <salmeen.m.s.m2002@gmail.com>
2026-03-12 02:27:18 +0300

    Banner Logo Updated 3

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -132,7 +132,7 @@ const HeroSection = () => {
         {/* Logo Large */}
         <div className="mb-8">
           <img 
-            src="updatedLogo.jpg" 
+            src="updatedLogo.jpeg" 
             alt="SGYN" 
             className="h-24 md:h-32 w-auto object-contain"
           />
