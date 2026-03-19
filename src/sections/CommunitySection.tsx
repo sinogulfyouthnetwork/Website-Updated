61388956eaad30a3cd06790b956fa0f0066ab5ed
salmeenbm <salmeen.m.s.m2002@gmail.com>
2026-03-19 19:15:54 +0300

    Community Members Image Updated

--- src/sections/CommunitySection.tsx
+++ src/sections/CommunitySection.tsx
@@ -91,7 +91,7 @@ const CommunitySection = () => {
       {/* Background Image */}
       <div className="community-image absolute inset-0">
         <img 
-          src="community_bottom_wide_event.jpg"
+          src="Members-Image.JPG"
           alt="SGYN Community"
           className="w-full h-full object-cover opacity-40"
         />
